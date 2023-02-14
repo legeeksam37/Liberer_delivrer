56e58a3438d435ceef7aefdb5c83261f43b7965e
Romain Pitot <romain.pitot@e-artsup.net>
2023-02-14 17:29:01 +0100

    update

--- Assets/QuizzInteraction.cs
+++ Assets/QuizzInteraction.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class QuizzInteraction : MonoBehaviour
 {
+    bool bouton1;
+    bool bouton2;
+    bool bouton3;
 
-    bool isFalse;
-    bool isRight;
+    bool skip;
+    bool change;
     public GameObject WinPannel;
     public GameObject LoosePannel;
     public GameObject BasePannel;
@@ -14,10 +17,83 @@ public class QuizzInteraction : MonoBehaviour
 
     void Update()
     {
-        if (isFalse==true)
+        if (bouton1 == true)
         {
+            Debug.Log("bouton1");
+            BasePannel.SetActive(false);
+            LoosePannel.SetActive(true);
+            change = true;
+
+            if(change == true)
+            {
+            skip = false;
+            change = false;
+            bouton1 = false;
+            }
+        }
+
+
+
+
+        if (bouton2 == true)
+        {
+            if(skip == true)
+            {
+                Debug.Log("bouton2");
+                BasePannel.SetActive(false);
+                LoosePannel.SetActive(true);
+                change = true;
+            }
+
+            if(change == true)
+            {
+                skip = false;
+                change = false;
+                bouton2 = true;
+            }
+        }
+
+
+
+        if (bouton3 == true)
+        {
+        if(skip == true)
+        {
+            Debug.Log("bouton3");
             BasePannel.SetActive(false);
             WinPannel.SetActive(true);
+            change = true;
         }
+        if(change == true)
+        {
+            skip = false;
+            change = false;
+            bouton3 = true;
+        }
+
+    }
+}
+
+    public void Skip()
+    {
+        //Debug.Log(skip);
+        skip = true;
+    }
+
+    public void next1()
+    {
+        bouton1 = true;
+    }
+
+        
+    public void next2()
+    {
+      bouton2 = true;
+    }
+
+
+    public void next3() // bonne rép
+    {
+      bouton3 = true;
     }
 }
