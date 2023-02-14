eaf0039f3070dfc74d7efd404bea47681f942c41
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-14 11:54:13 +0100

    Added missionManager

--- Assets/Scenes/Scripts/ChoiceTree/Mission.cs
+++ Assets/Scenes/Scripts/ChoiceTree/Mission.cs
@@ -16,29 +16,34 @@ public class Mission : ScriptableObject
     {
         _current = new RecursiveEnabledChoice(_rootChoice, new HashSet<Node>());
     }
-
-
-
-    /*private VisualNovelManager _manager;
-    private void Awake()
+    /// <summary>
+    /// Process sequence based on absolute branche index, not considering the enabled or disabled one; you can possibly call a disabled one, but a fixed option will always have same index
+    /// </summary>
+    /// <param name="branchIndex"></param>
+    public void ProcessSequenceAbsolute(int branchIndex)
     {
-        _manager = GameObject.Instantiate<VisualNovelManager>(null);
-        _manager.enabled = false;
-    }*/
-    public void ProcessSequence(int branchIndex)
-    {
-        if (branchIndex < 0 || branchIndex > _current._subChoices.Count || !_current._subChoices[branchIndex].enabled)
-            Debug.LogError("Wrong path, existing ? : " + !(branchIndex < 0 || branchIndex > _current._subChoices.Count)
-                + " choice available/enabled in current mission ? : " + _current._subChoices[branchIndex].enabled
-                + " Expect weird things to happen from now, good luck my friends...");
-        _current = _current._subChoices[branchIndex];
+        ProcessSequence(_current._subChoices, branchIndex);
     }
-
+    /// <summary>
+    /// Process sequence based on relative branche index, considering the enabled or disabled one; you will only call a valid one, but a fixed option will always have a different index someTimes
+    /// </summary>
+    /// <param name="branchIndex"></param>
+    public void ProcessSequenceRelative(int branchIndex)
+    {
+        ProcessSequence(_current._subChoices.Where(s => s.enabled).ToList(), branchIndex);
+    }
+    private void ProcessSequence(List<RecursiveEnabledChoice> choices, int branchIndex)
+    {
+        if (branchIndex < 0 || branchIndex > choices.Count || !choices[branchIndex].enabled)
+            Debug.LogError("Wrong path, existing ? : " + !(branchIndex < 0 || branchIndex > choices.Count)
+                + " choice available/enabled in current mission ? : " + choices[branchIndex].enabled
+                + " Expect weird things to happen from now, good luck my friends...");
+        _current = choices[branchIndex];
+    }
     internal void OnValidate()
     {
         Debug.LogWarning("Clear list of sons when disabled");
     }
-
     public bool IsFinalNode
     {
         get
@@ -46,5 +51,4 @@ public class Mission : ScriptableObject
             return _current._subChoices.Count == 0 || (_current._subChoices.Count == 1 && _current._subChoices[0].choice is FinalNode);
         }
     }
-
 }
