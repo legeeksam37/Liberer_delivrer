1deeeeb4dabdf148ec5a07c6ccef5821f736969f
legeeksam37 <65701874+legeeksam37@users.noreply.github.com>
2023-02-14 17:45:20 +0100

    homescreen
    legal mentions

--- Assets/Scenes/Scripts/HomeSceneManager.cs
+++ Assets/Scenes/Scripts/HomeSceneManager.cs
@@ -6,19 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class HomeSceneManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject popup_legal_mentions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hidePopUp();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void loadScene(string s_name){
         SceneManager.LoadScene(s_name);
     }
+
+    public void showPopUp(){
+         popup_legal_mentions.gameObject.SetActive(true);
+    }
+
+    public void hidePopUp(){
+        popup_legal_mentions.gameObject.SetActive(false);
+    }
+
 }
