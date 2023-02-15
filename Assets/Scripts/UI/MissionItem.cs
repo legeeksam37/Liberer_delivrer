b8ce47e0e39fe5f19c295fb976d0289e23ca492f
GAMINGCAMPUS\lbriot <lbriot@gaming.tech>
2023-02-15 10:58:03 +0100

    [feat]  - Level & Mission selection working - Lock & Unlock missions at runtime

--- Assets/Scripts/UI/MissionItem.cs
+++ Assets/Scripts/UI/MissionItem.cs
@@ -2,33 +2,74 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
 public class MissionItem : MonoBehaviour
 {
     public bool isLocked = false;
-    
     [SerializeField] private Image lockImage;
     
     private Button _missionsButton;
+
+    private delegate void LockStateChanged();
+
+    private event LockStateChanged onLockStateChanged;
+
+    [SerializeField]
+    private ScenesManager.Scene sceneToLoad = ScenesManager.Scene.Game;
+    
     private void Awake()
     {
         _missionsButton = GetComponent<Button>();
+        _missionsButton.onClick.AddListener(LoadSceneFromManager);
+    }
+
+    private void OnEnable()
+    {
+        onLockStateChanged += SetLockImageVisibility;
+        onLockStateChanged += SetButtonInteractability;
         ToggleLockMission();
     }
 
-    void ToggleLockMission()
+    private void OnDisable()
+    {
+        onLockStateChanged -= SetLockImageVisibility;
+        onLockStateChanged -= SetButtonInteractability;
+    }
+
+    private void Update()
+    {
+        SetLockImageVisibility();
+        SetButtonInteractability();
+    }
+
+    private bool ToggleLockMission()
     {
         isLocked = !isLocked;
         if (lockImage == null)
         {
             Debug.LogWarning("No lock image set.");
-            return;
+            return false;
         }
-        
+        onLockStateChanged?.Invoke();
+        return isLocked;
+    }
+
+    private void SetLockImageVisibility()
+    {
         lockImage.gameObject.SetActive(isLocked);
+    }
+
+    private void SetButtonInteractability()
+    {
         _missionsButton.enabled = !isLocked;
+    }
+
+    private void LoadSceneFromManager()
+    {
+        ScenesManager.GetInstance().LoadScene(sceneToLoad);
     }
     
 }
