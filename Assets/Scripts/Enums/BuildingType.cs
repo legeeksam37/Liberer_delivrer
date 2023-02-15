ca7f9e4958bcf3e6fcc12a8f0dc15e19f558e024
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-15 17:50:17 +0100

    Added building types on building and prefabd them

--- Assets/Scripts/Enums/BuildingType.cs
+++ Assets/Scripts/Enums/BuildingType.cs
@@ -1,10 +1,12 @@
 public enum BuildingType
 {
-    None,
-    BookShop,
-    Videogame,
-    Restaurant,
-    Market,
-    Supermarket,
-    Clothes
+    PlayerAppartement=-1,
+    None=0,
+    BookShop=1,
+    Videogame=2,
+    Electro=3,
+    Restaurant=4,
+    Market=5,
+    Supermarket=6,
+    Clothes=7
 }
