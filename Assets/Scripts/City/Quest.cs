cbe0d2eb87b0bf7f7651eafa8a72191bcd840379
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-16 15:42:07 +0100

    Added placholder working for the transports

--- Assets/Scripts/City/Quest.cs
+++ Assets/Scripts/City/Quest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ public class Quest : MonoBehaviour
     [SerializeField]
     private bool _autoFollowNextObjective;
     private SpriteRenderer _sr;
-    public static  Color secondaryColor = new Color(35 / 255f, 241 / 255f, 0f);
+    public static Color secondaryColor = new Color(35 / 255f, 241 / 255f, 0f);
     private void Awake()
     {
         if (_autoFollowNextObjective)
@@ -15,7 +16,7 @@ public class Quest : MonoBehaviour
         _sr = GetComponentInChildren<SpriteRenderer>();
         hide();
     }
-    public void Custom(Color color,Vector3 scale)
+    public void Custom(Color color, Vector3 scale)
     {
         _sr.color = color;
         transform.localScale = scale;
@@ -33,6 +34,17 @@ public class Quest : MonoBehaviour
         //transform.position = building.TransformOverride.position;
     }
     public void callQuest(BuildingType target) => callQuest(FindBuilding(target));
-    public static BuildingID FindBuilding(BuildingType target) => FindObjectsOfType<BuildingID>().First(b => b.Type == target);
+    public static BuildingID FindBuilding(BuildingType target)
+    {
+        try
+        {
+            return FindObjectsOfType<BuildingID>().First(b => b.Type == target);
+        }
+        catch (InvalidOperationException e)
+        {
+            Debug.Log("With target : " + target + "not found, returning null thrown "+e.Message);
+            return null;
+        }
+    }
 
 }
