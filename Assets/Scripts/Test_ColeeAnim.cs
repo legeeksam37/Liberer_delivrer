4d5285a5429948d9714c1ec382ad1be596abfad9
Ryfast <nicolas.hirohiko@gmail.com>
2023-02-16 11:08:07 +0100

    Animation fini et réparé

--- Assets/Scripts/Test_ColeeAnim.cs
+++ Assets/Scripts/Test_ColeeAnim.cs
@@ -7,7 +7,7 @@ public class Test_ColeeAnim : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Animator>().SetTrigger("Text_PopUp");
+        GetComponent<Animator>().SetTrigger("AnimStart");
     }
 
     // Update is called once per frame
