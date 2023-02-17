9e99ddac74dea50fb4229a02457354053c8b4f4c
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-17 09:52:03 +0100

    Concluded merge

--- Assets/CharMov/JoystickControls.cs
+++ Assets/CharMov/JoystickControls.cs
@@ -9,18 +9,20 @@ public class JoystickControls : MonoBehaviour
     PlayerInput playerInput;
     private Rigidbody2D playerRgbd;
     private Vector3 playerVelocity;
-    private Animator playerAnimator;
-    private Rigidbody2D controller;
-    [SerializeField,Range(.1f,5f)]
-    private float playerSpeed=.5f;
+    [SerializeField, Range(.1f, 5f)]
+    private float playerSpeed = .5f;
 
     public Image image;
+
+    private Animator playerAnimator;
+    private float lastXJoystick = 0;
+    private float lastYJoystick = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-        controller = GetComponent<Rigidbody2D>();       
         playerRgbd = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
     }
@@ -34,29 +36,36 @@ public class JoystickControls : MonoBehaviour
         playerRgbd.MovePosition(playerRgbd.position + playerSpeed * Time.deltaTime * move);
         if (image != null)
         {
-            if (input.x > 0.2f || input.x < -0.2f || input.y > 0.2f || input.y < -0.2f)
+            if (input.x > 0.1f || input.x < -0.1f || input.y > 0.1f || input.y < -0.1f)
             {
-
                 image.CrossFadeAlpha(255, 1.0f, false);
-            }
-            else
-            {
-                //image.CrossFadeAlpha(0, 0.0f, false);
+                //Save the last pos of the player before reaching null point
+                lastXJoystick = input.x;
+                lastYJoystick = input.y;
 
                 if (playerAnimator)
                 {
-                    playerAnimator.SetFloat("MoveX",0);
-                    playerAnimator.SetFloat("MoveY",0);
+                    playerAnimator.SetFloat("MoveX", input.x);
+                    playerAnimator.SetFloat("MoveY", input.y);
+                }
+            }
+            else
+            {
+                image.CrossFadeAlpha(0, 0.0f, false);
+                if (playerAnimator)
+                {
+                    playerAnimator.SetFloat("MoveX", 0);
+                    playerAnimator.SetFloat("MoveY", 0);
                 }
                 //Save the value 
                 if (playerAnimator)
                 {
-                    playerAnimator.SetFloat("LastMoveX",lastXJoystick);
-                    playerAnimator.SetFloat("LastMoveY",lastYJoystick);
+                    playerAnimator.SetFloat("LastMoveX", lastXJoystick);
+                    playerAnimator.SetFloat("LastMoveY", lastYJoystick);
                 }
             }
+
+
         }
-       
-        
     }
 }
